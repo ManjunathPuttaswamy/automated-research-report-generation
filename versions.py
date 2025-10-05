74783476f0b0871b19e9760bb218a99815b73d63
Manjunath Puttaswamy <Manju_86@Outlook.com>
2025-10-05 14:43:59 +0530

    Changes Were Updated

---
+++ versions.py
@@ -0,0 +1,32 @@
+import importlib.metadata
+packages = [
+    "python-dotenv",
+    "fastapi",
+    "ipykernel",
+    "langgraph",
+    "langchain",
+    "langchain-astradb",
+    "langchain-core",          # or "langchain_core" if metadata complains
+    "langchain-google-genai",
+    "langchain-groq",
+    "langchain-openai",
+    "langchain-mcp-adapters",
+    "mcp",
+    "langchain_community",
+    "structlog",
+    "langchain_core",
+    "tavily-python",           # if error, try "tavily_python"
+    "wikipedia",
+    "pydantic"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        
+        print(f"{pkg} (not installed)")
+
+# # serve static & templates
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+# templates = Jinja2Templates(directory="../templates")
